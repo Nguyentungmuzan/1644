acb07ed1f81c8f31b9aa3ea45a2d6af8dd0109ab
ANPHATPC\ADMIN <minhttgch210293@fpt.edu.vn>
2023-01-11 22:35:33 +0700

    maybe connect to db

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const mongo = require("mongodb").MongoClient;
+const url = "mongodb+srv://1644.8hjg4.mongodb.net/1644";
 dotenv.config();
 
-//port
-const port = process.env.PORT || 3333;
 const main = async () => {
   const app = express();
 
@@ -14,19 +14,35 @@ const main = async () => {
   app.use(morgan("tiny"));
   app.use(express.json());
 
-  app.use("/", (req, res) => {
+  app.use("/login", (req, res) => {
     res.json({
-      message: "Du moa chung may",
+      message: "Login page",
     });
   });
 
   app.use("*", (req, res) => {
     res.json({
-      message: "This url does not exist",
+      message: "Another page",
     });
   });
 
-  app.listen(port, () => {
+  //Allow origin
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+  })
+
+  app.listen(process.env.NODE_PORT, () => {
+    mongo.connect(url, { useNewUrlParser: true }, (err, db) => {
+      if (err) {
+       console.log(err);
+       process.exit(0);
+       }
+      console.log("Database connected!");
+       db.close();
+      });      
     console.log(`Server is listening on http://${process.env.NODE_HOST}:${process.env.NODE_PORT}`);
   });
 };
---
+++ src/models/model.js
@@ -0,0 +1,10 @@
+// const mongo = require("mongodb");
+// const url = "mongodb://localhost:27017/1644";
+// mongo.connect(url, { useNewUrlParser: true }, (err, db) => {
+// if (err) {
+//  console.log(err);
+//  process.exit(0);
+//  }
+// console.log("database connected!");
+//  db.close();
+// });
